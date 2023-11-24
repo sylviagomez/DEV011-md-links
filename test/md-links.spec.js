20956b9c72085caeb6468ae3168636aea6c2d55d
Sylvia Gomez <syl.gomez.bio@gmail.com>
2023-11-24 10:30:09 -0600

    fixes #24 y #26

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -10,45 +10,38 @@ describe('mdLinks', () => {
     const result = mdLinks(filePath);
     expect(result).toBeInstanceOf(Promise);
   });
-
-  // it('debería resolver con los enlaces del archivo Markdown correctamente', () => {
-  //   const filePath = 'test/archivo-prueba.md'; 
-  //   const expectedLinks = {"href": "https://www.google.com/", "text": "https://www.google.com/", "title": "C:\\Users\\vicia\\Documents\\Laboratoria\\DEV011-md-links\\test\\archivo-prueba.md"};
-  //   expect(mdLinks(filePath)).resolves.toEqual([expectedLinks]);  
-  // });
-
   it('debería rechazar con un error para una ruta inexistente', () => {
     const nonExistingPath = '/ruta/inexistente'; 
     expect(mdLinks(nonExistingPath)).rejects.toThrowError('La ruta no existe');
   });
 
-  it('debería rechazar con un error para un archivo no Markdown', () => {
+  it('debería rechazar con un error para un archivo que no es Markdown', () => {
     const filePath = 'C:\\Users\\vicia\\Documents\\Laboratoria\\DEV011-md-links\\test\\README.js'; 
     expect(mdLinks(filePath)).rejects.toThrowError('La ruta no es un archivo Markdown'); 
 });
 
 it('debería resolver con los enlaces del archivo Markdown correctamente (sin validación)', () => {
   const filePath = 'test/archivo-prueba.md';
-  const expectedLinks = {
-    href: 'https://www.google.com/',
-    text: 'https://www.google.com/',
-    title: 'C:\\Users\\vicia\\Documents\\Laboratoria\\DEV011-md-links\\test\\archivo-prueba.md',
-  };
+  const expectedLinks = {"links": 
+  [{"href": "https://www.google.com/", 
+  "text": "https://www.google.com/", 
+  "title": "C:\\Users\\vicia\\Documents\\Laboratoria\\DEV011-md-links\\test\\archivo-prueba.md"}], 
+  "stats": {"total": 1, "unique": 1}
+  }
 
-  return expect(mdLinks(filePath, false)).resolves.toEqual([expectedLinks]);
+  return expect(mdLinks(filePath, false)).resolves.toEqual(expectedLinks);
 });
 
 it('debería resolver con los enlaces del archivo Markdown correctamente (con validación)', () => {
   const filePath = 'test/archivo-prueba.md';
-  const expectedValidatedLinks = {
-    href: 'https://www.google.com/',
-    text: 'https://www.google.com/',
-    title: 'C:\\Users\\vicia\\Documents\\Laboratoria\\DEV011-md-links\\test\\archivo-prueba.md',
-    status: 200,
-    ok: 'OK',
-  };
-
-  return expect(mdLinks(filePath, true)).resolves.toEqual([expectedValidatedLinks]);
+  const expectedValidatedLinks = {"links": 
+  [{"href": "https://www.google.com/", 
+  "ok": "OK", "status": 200, 
+  "text": "https://www.google.com/", 
+  "title": "C:\\Users\\vicia\\Documents\\Laboratoria\\DEV011-md-links\\test\\archivo-prueba.md"}], 
+  "stats": {"broken": 0, "total": 1, "unique": 1}
+  }
+  return expect(mdLinks(filePath, true)).resolves.toEqual(expectedValidatedLinks);
 });
 
 })
